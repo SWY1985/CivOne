23daa6ee6be55deaf99ff86255c992f3675560c0
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-13 15:58:56 +0200

    Added Demo screen to window

--- src/Window.cs
+++ src/Window.cs
@@ -1,25 +1,93 @@
-/* CivOne
- *
- * To the extent possible under law, the person who associated CC0 with
- * CivOne has waived all copyright and related or neighboring rights
- * to CivOne.
- *
- * You should have received a copy of the CC0 legalcode along with this
- * work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using CivOne.Enums;
+using CivOne.GFX;
+using CivOne.Interfaces;
+using CivOne.Screens;
 
 namespace CivOne
 {
 	internal class Window : Form
 	{
+		private readonly List<IScreen> _screens = new List<IScreen>();
+		
+		private Cursor _hiddenCursor;
+		private Cursor[,] _cursorPointer,_cursorGoto;
+		private MouseCursor _currentCursor = MouseCursor.Pointer;
+		
+        private void LoadCursor(ref Cursor[,] cursor, int x, int y)
+        {
+            int scale = Settings.Instance.Scale;
+            cursor = new Cursor[scale, scale];
+            Bitmap img = Resources.Instance.GetPart("SP257", x, y, 16, 16);
+
+            for (int cx = 0; cx < scale; cx++)
+                for (int cy = 0; cy < scale; cy++)
+                {
+                    Bitmap res = new Bitmap(32 * scale, 32 * scale, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                    Graphics gfx = Graphics.FromImage(res);
+                    gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    gfx.PixelOffsetMode = PixelOffsetMode.Half;
+                    gfx.DrawImage(img, (15 * scale) - cx, (15 * scale) - cy, 16 * scale, 16 * scale);
+                    cursor[cx, cy] = new Cursor(res.GetHicon());
+                }
+        }
+		
+		private void OnLoad(object sender, EventArgs args)
+		{
+			// Load the demo
+			_screens.Add(new Demo());
+			
+            // Load cursors
+            _hiddenCursor = new Cursor(new Bitmap(16, 16).GetHicon());
+            LoadCursor(ref _cursorPointer, 112, 32);
+            LoadCursor(ref _cursorGoto, 32, 32);
+		}
+		
 		private void OnPaint(object sender, PaintEventArgs args)
 		{
-			args.Graphics.Clear(Color.Black);
+			args.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+			args.Graphics.PixelOffsetMode = PixelOffsetMode.Half;
+			
+			foreach (IScreen screen in _screens)
+			{
+				screen.Draw(args.Graphics);
+			}
 		}
+		
+        private void OnMouseMove(object sender, MouseEventArgs args)
+        {
+            if (_currentCursor == MouseCursor.None)
+            {
+                Cursor = _hiddenCursor;
+                return;
+            }
+
+            // apply cursor correction
+            int s = Settings.Instance.Scale;
+            if (args.X < 0 || args.Y < 0) return;
+            switch (_currentCursor)
+            {
+                case MouseCursor.Pointer:
+                    Cursor = _cursorPointer[args.X % s, args.Y % s];
+                    break;
+                case MouseCursor.Goto:
+                    Cursor = _cursorGoto[args.X % s, args.Y % s];
+                    break;
+            }
+        }
 		
 		public Window()
 		{
@@ -29,11 +97,13 @@ namespace CivOne
 			DoubleBuffered = true;
 			FormBorderStyle = FormBorderStyle.FixedSingle;
 			MaximizeBox = false;
-			ClientSize = new Size(320 * 2, 200 * 2);
+			ClientSize = new Size(320 * Settings.Instance.Scale, 200 * Settings.Instance.Scale);
 			Text = "CivOne";
 			
 			// Set Window events
+			Load += OnLoad;
 			Paint += OnPaint;
+			MouseMove += OnMouseMove;
 			
 			ResumeLayout(false);
 		}
