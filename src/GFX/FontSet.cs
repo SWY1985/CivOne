65298ad6581e5a3f128b07e45954d3941a3d9454
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-13 15:56:50 +0200

    Added GFX Namespace
    The GFX Namespace is used to draw graphics and store graphics caches

---
+++ src/GFX/FontSet.cs
@@ -0,0 +1,121 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace CivOne.GFX
+{
+	internal class Fontset
+	{
+		private readonly byte _fontAsciiFirst;
+		private readonly byte _fontAsciiLast;
+		private readonly byte _charByteLength;
+		private readonly byte _charTopRow;
+		private readonly byte _charBottomRow;
+		private readonly byte _fontSpaceX;
+		private readonly byte _fontSpaceY;
+		private readonly Color[] _colours;
+		private Dictionary<char, byte> _charWidths = new Dictionary<char, byte>();
+		private Dictionary<char, byte[]> _characters = new Dictionary<char, byte[]>();
+
+		public Bitmap GetLetter(char character, byte colour)
+		{
+			if (!_charWidths.ContainsKey(character) || !_characters.ContainsKey(character)) return new Bitmap(8, 8);
+			int ww = _charWidths[character], hh = 1 + _charBottomRow - _charTopRow;
+
+			byte[] pixels = new byte[ww * hh];
+			int index = 0, b = 0, bit = 0;;
+			for (int y = 0; y < 1 + _charBottomRow - _charTopRow; y++)
+			{
+				if (bit > 0)
+				{
+					bit = 0;
+					b++;
+				}
+				if (_charByteLength == 2)
+				{
+				}
+				for (int x = 0; x < _charByteLength * 8; x++)
+				{
+					if (x < _charWidths[character])
+					{
+						if ((_characters[character][b] & (0x80 >> bit)) > 0)
+						{
+							pixels[index++] = colour;
+						}
+						else
+						{
+							pixels[index++] = 0;
+						}
+					}
+
+					if (++bit == 8)
+					{
+						bit = 0;
+						b++;
+					}
+				}
+			}
+			
+			Bitmap output = new Bitmap(ww, hh, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+			for (int y = 0; y < hh; y++)
+			{
+				BitmapData bmpData = output.LockBits(new Rectangle(0, y, ww, 1), ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+				Marshal.Copy(pixels, (ww * y), bmpData.Scan0, ww);
+				output.UnlockBits(bmpData);
+			}
+
+			ColorPalette palette = output.Palette;
+			for (int i = 0; i < _colours.Length; i++)
+				palette.Entries[i] = _colours[i];
+			output.Palette = palette;
+
+			return output;
+		}
+
+		public Fontset(byte[] bytes, ushort offset, Color[] palette)
+		{
+			_colours = palette;
+
+			_fontAsciiFirst = bytes[offset - 8];
+			_fontAsciiLast = bytes[offset - 7];
+			_charByteLength = bytes[offset - 6];
+			_charTopRow = bytes[offset - 5];
+			_charBottomRow = bytes[offset - 4];
+			_fontSpaceX = bytes[offset - 3];
+			_fontSpaceY = bytes[offset - 2];
+
+			int i = 0;
+			int index = (int)offset;
+			int charCount = 1 + (_fontAsciiLast - _fontAsciiFirst);
+			for (int c = _fontAsciiFirst; c <= _fontAsciiLast; c++)
+			{
+				int ww = i++;
+				char character = (char)c;
+				byte[] b = new byte[(1 + _charBottomRow - _charTopRow) * _charByteLength];
+				for (int row = 0; row < (1 + _charBottomRow - _charTopRow); row++)
+					for (int col = 0; col < _charByteLength; col++)
+					{
+						int ind = (row * _charByteLength) + col;
+						int bin = index + (row * (_charByteLength * charCount)) + col;
+
+						b[ind] = bytes[bin];
+					}
+				_characters.Add(character, b);
+				_charWidths.Add(character, bytes[offset - 9 - charCount + i]);
+
+				index += _charByteLength;
+			}
+		}
+	}
+}
---
+++ src/GFX/Resources.cs
@@ -0,0 +1,162 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using CivOne.Enums;
+using CivOne.IO;
+
+namespace CivOne.GFX
+{
+	internal class Resources
+	{
+		private readonly Dictionary<string, Picture> _cache = new Dictionary<string, Picture>();
+		private readonly Dictionary<string, Bitmap> _textCache = new Dictionary<string, Bitmap>();
+		private readonly List<Fontset> _fonts = new List<Fontset>();
+
+		private void LoadFonts()
+		{
+			byte[] file;
+			using (FileStream fs = new FileStream(Path.Combine(Settings.Instance.DataDirectory, "FONTS.CV"), FileMode.Open))
+			{
+				file = new byte[fs.Length];
+				for (int i = 0; i < fs.Length; i++) file[i] = (byte)fs.ReadByte();
+			}
+
+			List<ushort> fontOffsets = new List<ushort>();
+			int index = 0;
+			uint fontCount = BitConverter.ToUInt16(file, index);
+			index += 2;
+
+			for (int i = 0; i < fontCount; i++)
+			{
+				fontOffsets.Add(BitConverter.ToUInt16(file, index));
+				index += 2;
+			}
+
+			foreach (ushort offset in fontOffsets)
+			{
+				_fonts.Add(new Fontset(file, offset, LoadPIC("SP257").Image.Palette.Entries));
+			}
+		}
+
+		public Size GetTextSize(int font, string text)
+		{
+			int width = 0, height = 0;
+			foreach (char c in text)
+			{
+				Size size = GetLetterSize(font, c);
+				width += size.Width + 1;
+				if (height < size.Height) height = size.Height;
+			}
+			return new Size(width, height);
+		}
+
+		public Bitmap GetText(string text, int font, byte colour)
+		{
+			return GetText(text, font, colour, colour);
+		}
+
+		public Bitmap GetText(string text, int font, byte colourFirstLetter, byte colour)
+		{
+			List<Bitmap> letters = new List<Bitmap>();
+			bool isFirstLetter = true;
+			foreach (char c in text)
+			{
+				letters.Add(GetLetter(isFirstLetter ? colourFirstLetter : colour, font, c));
+				isFirstLetter = false;
+			}
+
+			int width = 0, height = 0;
+			foreach (Bitmap letter in letters)
+			{
+				width += letter.Width + 1;
+				if (height < letter.Height) height = letter.Height;
+			}
+
+			Bitmap output = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+
+			int xx = 0;
+			foreach (Bitmap letter in letters)
+			{
+				output = Picture.Combine(output, letter, new Point(xx, 0));
+				xx += letter.Width + 1;
+			}
+
+			output.Palette = letters[0].Palette;
+
+			return output;
+		}
+
+		private Size GetLetterSize(int font, char letter)
+		{
+			return GetLetter(5, font, letter).Size;
+		}
+
+		private Bitmap GetLetter(byte colour, int font, char letter)
+		{
+			string key = string.Format("letter{0}|{1}|{2}", colour, font, letter);
+			if (!_textCache.ContainsKey(key))
+			{
+				_textCache.Add(key, _fonts[font].GetLetter(letter, colour));
+			}
+			return _textCache[key];
+		}
+		
+		public Bitmap GetPart(string filename, int x, int y, int width, int height)
+		{
+			return LoadPIC(filename).GetPart(x, y, width, height);
+		}
+
+		public Picture LoadPIC(string filename, bool noCache = false)
+		{
+			string key = filename.ToUpper();
+			if (_cache.ContainsKey(key))
+			{
+				if (!noCache) return _cache[key];
+				_cache.Remove(key);
+			}
+			
+			Picture output = null;
+			PicFile picFile = new PicFile(filename);
+			if (Settings.Instance.GraphicsMode == GraphicsMode.Graphics256 || picFile.GetPicture16 == null)
+			{
+				output = new Picture(picFile.GetPicture256, picFile.GetPalette256);
+			}
+			else
+			{
+				output = new Picture(picFile.GetPicture16, picFile.GetPalette16);
+			}
+
+			_cache.Add(key, output);
+			return output;
+		}
+		
+		private static Resources _instance;
+		public static Resources Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new Resources();
+				}
+				return _instance;
+			}
+		}
+
+		private Resources()
+		{
+			LoadFonts();
+		}
+	}
+}
