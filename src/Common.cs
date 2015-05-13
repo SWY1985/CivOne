707d67eac4e6ad358d9617e8051dddff759de238
SWY1985 <mail@swouters.nl>
2015-05-13 15:58:00 +0200

    Added Settings/Common

---
+++ src/Common.cs
@@ -0,0 +1,48 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System.Drawing;
+
+namespace CivOne
+{
+	internal class Common
+	{
+		private static Color[] _palette16;
+		public static Color[] GetPalette16
+		{
+			get
+			{
+				if (_palette16 == null)
+				{
+					byte[] shades = new byte[] { 0, 104, 183, 255 };
+					_palette16 = new Color[]
+					{
+						Color.Transparent,
+						Color.FromArgb(shades[0], shades[0], shades[2]),
+						Color.FromArgb(shades[0], shades[2], shades[0]),
+						Color.FromArgb(shades[0], shades[2], shades[2]),
+						Color.FromArgb(shades[2], shades[0], shades[0]),
+						Color.FromArgb(shades[0], shades[0], shades[0]),
+						Color.FromArgb(shades[2], shades[1], shades[0]),
+						Color.FromArgb(shades[2], shades[2], shades[2]),
+						Color.FromArgb(shades[1], shades[1], shades[1]),
+						Color.FromArgb(shades[1], shades[1], shades[3]),
+						Color.FromArgb(shades[1], shades[3], shades[1]),
+						Color.FromArgb(shades[1], shades[3], shades[3]),
+						Color.FromArgb(shades[3], shades[1], shades[1]),
+						Color.FromArgb(shades[3], shades[1], shades[3]),
+						Color.FromArgb(shades[3], shades[3], shades[1]),
+						Color.FromArgb(shades[3], shades[3], shades[3]),
+					};
+				}
+				return _palette16;
+			}
+		}
+	}
+}
