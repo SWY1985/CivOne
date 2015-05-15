613fa08ba7cd60cdc00c8ccaea70cbf324d6d8d6
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-15 16:15:43 +0200

    Implemented credits screen

--- src/Screens/Credits.cs
+++ src/Screens/Credits.cs
@@ -7,36 +7,166 @@
 // You should have received a copy of the CC0 legalcode along with this
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+using System;
+using System.Drawing.Imaging;
+using System.Linq;
 using System.Windows.Forms;
 using CivOne.Enums;
 using CivOne.GFX;
+using CivOne.IO;
 using CivOne.Templates;
 
 namespace CivOne.Screens
 {
 	internal class Credits : BaseScreen
 	{
-		public override MouseCursor Cursor
+		private readonly int[] SHOW_INTRO_LINE = new[] { 312, 279, 254, 221, 196, 171, 146, 121, 96, 71, 46, 21, -4, -37, -62, -95, -120, -145, -170, -195, -220, -245, -270, -295 };
+		private readonly int[] HIDE_INTRO_LINE = new[] { 287, 229, -29, -87, -315 };
+		
+		private readonly byte[] _textColours;
+		private readonly string[] _introText;
+		private readonly Picture[] _pictures;
+		private readonly ColorPalette _palette;
+		private readonly byte[,] _noiseMap;
+		
+		private int _introLeft = 320;
+		private int _logoSwipe = 0;
+		private int _cycleCounter = 0;
+		private int _noiseCounter = 33;
+		
+		private bool _showIntroLine = false;
+		private bool _introSkipped = false;
+		private int _introLine = -1;
+		
+		private void HandleIntroText()
 		{
-			get
+			_introLeft--;
+			if (SHOW_INTRO_LINE.Contains(_introLeft))
 			{
-				return MouseCursor.Pointer;
+				_showIntroLine = true;
+				_introLine++;
+			}
+			else if (HIDE_INTRO_LINE.Contains(_introLeft))
+			{
+				_showIntroLine = false;
 			}
 		}
 		
 		public override bool HasUpdate(uint gameTick)
 		{
-			return false;
+            if (gameTick % 2 == 0 || _noiseCounter == 0) return false;	
+			
+			// Updates
+            if (_introLeft > -320)
+			{
+				HandleIntroText();
+			}
+			else if (_logoSwipe < 320)
+			{
+				_canvas.Cycle(224, 254);
+                _logoSwipe += 16;
+			}
+			else if (_cycleCounter < 98)
+			{
+				_canvas.Cycle(224, 254);
+				_cycleCounter++;
+			}
+			else if (_noiseCounter > 0)
+			{
+				_pictures[1].ApplyNoise(_noiseMap, --_noiseCounter);
+			}
+			
+			// Drawing
+			_canvas.FillRectangle(0, 0, 0, 320, 200);
+            if (_introLeft < 320 && _introLeft > 0)
+			{
+				_canvas.AddLayer(_pictures[0].GetPart(0, 0, 320 - _introLeft, 200), _introLeft);
+			}
+			if (_introLeft > -320 && _introLeft <= 0)
+			{
+				_canvas.AddLayer(_pictures[0].GetPart(-_introLeft, 0, 320 + _introLeft, 200), 0);
+				if (_introLeft < 0)
+					_canvas.AddLayer(_pictures[1].GetPart(0, 0, -_introLeft, 200), _introLeft + 320);
+			}
+			if (_introLeft > -320 && _showIntroLine)
+			{
+				_canvas.DrawText(_introText[_introLine], 4, _textColours[0], 160, 82, TextAlign.Center);
+				_canvas.DrawText(_introText[_introLine], 4, _textColours[1], 160, 84, TextAlign.Center);
+				_canvas.DrawText(_introText[_introLine], 4, _textColours[2], 160, 83, TextAlign.Center);
+			}
+			if (_introLeft == -320 && _noiseCounter > 0)
+            {
+                if (!_introSkipped) _canvas.AddLayer(_pictures[1].Image);
+				if (_logoSwipe < 320)
+				{
+					if (_logoSwipe > 0)
+					{
+						_canvas.AddLayer(_pictures[2].GetPart(0, 0, _logoSwipe, 200));
+					}
+				}
+				else
+				{
+					_canvas.AddLayer(_pictures[2].Image);	
+				}
+				if (_introSkipped) _canvas.AddLayer(_pictures[1].Image);
+            }
+			else if (_noiseCounter == 0)
+			{
+				_canvas.AddLayer(_pictures[2].Image);
+			}
+			return true;
+		}
+		
+		public bool SkipIntro()
+		{
+			if (_introSkipped || _noiseCounter < 33) return false;
+			
+			_showIntroLine = false;
+			_introLeft = -320;
+			_logoSwipe = 320;
+			_cycleCounter = 98;
+			_pictures[1].FillRectangle(5, 0, 0, 320, 200);
+			_introSkipped = true;
+			return true;
 		}
 		
 		public override bool KeyDown(KeyEventArgs args)
 		{
-			return false;
+			return SkipIntro();
 		}
 		
 		public override bool MouseDown(MouseEventArgs args)
 		{
-			return false;
+			return SkipIntro();
+		}
+		
+		public Credits()
+		{
+            _introText = TextFile.Instance.LoadArray("credits");
+            _pictures = new Picture[3];
+            for (int i = 0; i < 2; i++)
+                _pictures[i] = Resources.Instance.LoadPIC(string.Format("BIRTH{0}", i));
+            _pictures[2] = Resources.Instance.LoadPIC("LOGO");
+            _palette = _pictures[0].Image.Palette;
+            _noiseMap = new byte[320, 200];
+            for (int x = 0; x < 320; x++)
+            {
+                for (int y = 0; y < 200; y++)
+                {
+                    _noiseMap[x, y] = (byte)Common.Random.Next(1, _noiseCounter);
+                }
+            }
+			switch (Settings.Instance.GraphicsMode)
+			{
+				case GraphicsMode.Graphics256:
+					_textColours = new byte[] { 247, 252, 250, 5, 229 };
+					break;
+				case GraphicsMode.Graphics16:
+					_textColours = new byte[] { 15, 15, 7, 5, 8 };
+					break;
+			}
+			
+			_canvas = new Picture(320, 200, _pictures[2].Image.Palette.Entries);
 		}
 	}
 }
