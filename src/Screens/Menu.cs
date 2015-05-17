33aadd568b41d2e71ca6d563ed6e52be698ddd52
SWY1985 <mail@swouters.nl>
2015-05-17 11:24:34 +0200

    Finished Credits Screen, FPS fix, added Menu Screen

---
+++ src/Screens/Menu.cs
@@ -0,0 +1,43 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using CivOne.Enums;
+using CivOne.GFX;
+using CivOne.Templates;
+
+namespace CivOne.Screens
+{
+	internal class Menu : BaseScreen
+	{
+		public override bool HasUpdate(uint gameTick)
+		{
+			return false;
+		}
+		
+		public override bool KeyDown(KeyEventArgs args)
+		{
+			return false;
+		}
+		
+		public override bool MouseDown(MouseEventArgs args)
+		{
+			return false;
+		}
+		
+		public Menu(Color[] colours)
+		{
+			Cursor = MouseCursor.Pointer;
+			
+			_canvas = new Picture(320, 200, colours);
+		}
+	}
+}
