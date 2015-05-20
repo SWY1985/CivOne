e8fb7bc6378bf3c9a87d342faf3ee81ee8a95835
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-20 06:36:54 +0200

    Added classes for all tile types

---
+++ src/Tiles/Ocean.cs
@@ -0,0 +1,23 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Enums;
+using CivOne.Templates;
+
+namespace CivOne.Tiles
+{
+	internal class Ocean : BaseTile
+	{
+		public Ocean(int x, int y, bool special) : base(x, y, special)
+		{
+			Type = Terrain.Ocean;
+			Name = "Ocean";
+		}
+	}
+}
