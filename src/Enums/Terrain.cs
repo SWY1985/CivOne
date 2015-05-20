91c60467e29666df1e6815f965d44bc5b645ee2a
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-20 06:36:02 +0200

    Added Terrain Enum

---
+++ src/Enums/Terrain.cs
@@ -0,0 +1,29 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+namespace CivOne.Enums
+{
+	public enum Terrain
+	{
+		None = -1,
+		Desert = 0,
+		Plains = 1,
+		Grassland1 = 2,
+		Forest = 3,
+		Hills = 4,
+		Mountains = 5,
+		Tundra = 6,
+		Arctic = 7,
+		Swamp = 8,
+		Jungle = 9,
+		Ocean = 10,
+		River = 11,
+		Grassland2 = 12,
+	}
+}
