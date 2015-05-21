f9791329f3b8ab58b26958b08d9709da830ef16f
SWY1985 <mail@swouters.nl>
2015-05-21 08:08:58 +0200

    Added Player class

---
+++ src/Player.cs
@@ -0,0 +1,58 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Interfaces;
+
+namespace CivOne
+{
+	internal class Player
+	{
+		private readonly ICivilization _civilization;
+		public ICivilization Civilization
+		{
+			get
+			{
+				return _civilization;
+			}
+		}
+		
+		public string LeaderName
+		{
+			get
+			{
+				// TODO: Implement custom leader name
+				return _civilization.LeaderName;
+			}
+		}
+		
+		public string TribeName
+		{
+			get
+			{
+				// TODO: Implement custom civilization name
+				return _civilization.Name;
+			}
+		}
+		
+		public string TribeNamePlural
+		{
+			get
+			{
+				// TODO: Implement custom civilization name
+				return _civilization.NamePlural;
+			}
+		}
+		
+		public Player(ICivilization civilization, string customLeaderName = null, string customCivilizationName = null)
+		{
+			// TODO: Implement custom leader name and custom civilization name
+			
+		}
+	}
+}
