be92407856db845ad9150afb400509db8dc55f89
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-21 08:09:12 +0200

    Added parameters to Game class

--- src/Game.cs
+++ src/Game.cs
@@ -8,23 +8,36 @@
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System;
+using CivOne.Interfaces;
 
 namespace CivOne
 {
 	internal class Game
 	{
+		public static void CreateGame(int difficulty, int competition, ICivilization tribe)
+		{
+			if (_instance == null)
+			{
+				Console.WriteLine("ERROR: Game instance already exists");
+				return;
+			}
+			_instance = new Game(difficulty, competition, tribe);
+		}
+		
 		private static Game _instance;
 		public static Game Instance
 		{
 			get
 			{
 				if (_instance == null)
-					_instance = new Game();
+				{
+					Console.WriteLine("ERROR: Game instance does not exist");
+				}
 				return _instance;
 			}
 		}
 		
-		private Game()
+		private Game(int difficulty, int competition, ICivilization tribe)
 		{
 			Console.WriteLine("Game instance created");
 		}
