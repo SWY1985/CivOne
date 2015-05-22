1561a0dc63a94de460d8afa01fc5a9f086b4b64c
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-22 22:39:40 +0200

    Finished sidebar

--- src/Screens/SideBar.cs
+++ src/Screens/SideBar.cs
@@ -8,6 +8,7 @@
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System.Drawing;
+using CivOne.Enums;
 using CivOne.GFX;
 using CivOne.Templates;
 
@@ -16,6 +17,8 @@ namespace CivOne.Screens
 	internal class SideBar : BaseScreen
 	{
 		private bool _update = true;
+		
+		private readonly Picture _miniMap, _demographics, _gameInfo;
 		
 		public override bool HasUpdate(uint gameTick)
 		{
@@ -31,8 +34,32 @@ namespace CivOne.Screens
 		{
 			Bitmap background = Resources.Instance.GetPart("SP299", 288, 120, 32, 16);
 			
+			_miniMap = new Picture(80, 50, palette);
+			_miniMap.FillRectangle(5, 0, 0, 80, 50);
+			_miniMap.AddBorder(15, 8, 0, 0, 80, 50);
+			
+			_demographics = new Picture(80, 39, palette);
+			_demographics.FillLayerTile(background);
+			_demographics.AddBorder(15, 8, 0, 0, 80, 39);
+			_demographics.FillRectangle(11, 3, 2, 74, 11);
+			_demographics.FillRectangle(2, 3, 13, 74, 1);
+			_demographics.DrawText("10,000 #", 0, 5, 2, 15, TextAlign.Left);
+			_demographics.DrawText("4000 B.C", 0, 5, 2, 23, TextAlign.Left);
+			_demographics.DrawText("0$ 0.5.5", 0, 5, 2, 31, TextAlign.Left);
+			
+			_gameInfo = new Picture(80, 103, palette);
+			_gameInfo.FillLayerTile(background);
+			_gameInfo.AddBorder(15, 8, 0, 0, 80, 103);
+			_gameInfo.DrawText(string.Format("({0})", Game.Instance.HumanPlayer.TribeName), 0, 5, 4, 2, TextAlign.Left);
+			_gameInfo.DrawText("Settlers", 0, 5, 4, 10, TextAlign.Left);
+			_gameInfo.DrawText("Moves: 1", 0, 5, 4, 18, TextAlign.Left);
+			_gameInfo.DrawText("NONE", 0, 5, 4, 26, TextAlign.Left);
+			_gameInfo.DrawText("(Plains)", 0, 5, 4, 34, TextAlign.Left);
+			
 			_canvas = new Picture(80, 192, palette);
-			_canvas.FillLayerTile(background);
+			_canvas.AddLayer(_miniMap.Image, 0, 0);
+			_canvas.AddLayer(_demographics.Image, 0, 50);
+			_canvas.AddLayer(_gameInfo.Image, 0, 89);
 		}
 	}
 }
