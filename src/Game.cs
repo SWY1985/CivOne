ecdda416abd4c2f266e54a7baa895b3adb408f90
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-05-23 11:06:17 +0200

    Added Player creation to Game Instance

--- src/Game.cs
+++ src/Game.cs
@@ -8,12 +8,17 @@
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System;
+using System.Linq;
+using CivOne.Civilizations;
 using CivOne.Interfaces;
 
 namespace CivOne
 {
 	internal class Game
 	{
+		private readonly int _difficulty, _competition;
+		private readonly Player[] _players;
+		
 		internal Player HumanPlayer
 		{
 			get;
@@ -45,8 +50,27 @@ namespace CivOne
 		
 		private Game(int difficulty, int competition, ICivilization tribe)
 		{
-			Console.WriteLine("Game instance created");
-			HumanPlayer = new Player(tribe);
+			_difficulty = difficulty;
+			_competition = competition;
+			Console.WriteLine("Game instance created (difficulty: {0}, competition: {1})", _difficulty, _competition);
+			
+			_players = new Player[competition + 1];
+			for (int i = 0; i <= competition; i++)
+			{
+				if (i == tribe.PreferredPlayerNumber)
+				{
+					_players[i] = new Player(tribe);
+					HumanPlayer = _players[i];
+					Console.WriteLine("- Player {0} is {1} of the {2} (human)", i, _players[i].Civilization.LeaderName, _players[i].Civilization.NamePlural);
+					continue;
+				}
+				
+				ICivilization[] civs = Common.Civilizations.Where(civ => civ.PreferredPlayerNumber == i).ToArray();
+				int r = Common.Random.Next(0, civs.Length);
+				
+				_players[i] = new Player(civs[r]);
+				Console.WriteLine("- Player {0} is {1} of the {2}", i, _players[i].Civilization.LeaderName, _players[i].Civilization.NamePlural);
+			}
 		}
 	}
 }
