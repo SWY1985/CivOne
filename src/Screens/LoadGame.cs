541bdf3f961e14b7a0dbe6085e15bbc3da8f5753
SWY1985 <mail@swouters.nl>
2015-05-27 16:29:37 +0200

    Load game menu correctly shows SVE game names

--- src/Screens/LoadGame.cs
+++ src/Screens/LoadGame.cs
@@ -8,7 +8,10 @@
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 using CivOne.Enums;
 using CivOne.GFX;
@@ -18,12 +21,91 @@ namespace CivOne.Screens
 {
 	internal class LoadGame : BaseScreen
 	{
+		private class SaveGameFile
+		{
+			// TODO: Implement full save file configuration
+			// - http://forums.civfanatics.com/showthread.php?p=12422448
+			// - http://forums.civfanatics.com/showthread.php?t=493581
+			
+			public bool ValidFile { get; private set; }
+			public string SveFile { get; private set; }
+			public string MapFile { get; private set; }
+			
+			public string Name { get; private set; }
+			
+			private string[] BytesToArray(byte[] bytes, int maxLength)
+			{
+				List<string> output = new List<string>();
+				StringBuilder sb = new StringBuilder();
+				foreach (byte b in bytes)
+				{
+					sb.Append((char)b);
+					if (sb.Length != maxLength) continue;
+					
+					output.Add(sb.ToString().Split((char)0)[0].Trim());
+					sb.Clear();
+				}
+				return output.ToArray();
+			}
+			
+			public SaveGameFile(string filename)
+			{
+				ValidFile = false;
+				Name = "(EMPTY)";
+				SveFile = string.Format("{0}.SVE", filename);
+				MapFile = string.Format("{0}.MAP", filename);
+				Console.WriteLine(SveFile);
+				if (!File.Exists(SveFile) || !File.Exists(MapFile)) return;
+				
+				using (BinaryReader br = new BinaryReader(File.Open(SveFile, FileMode.Open)))
+				{
+					ushort gameTurn = br.ReadUInt16();
+					ushort humanPlayer = br.ReadUInt16();
+					ushort humanPlayerBitflag = br.ReadUInt16();
+					ushort randomMapSeed = br.ReadUInt16();
+					ushort currentYear = br.ReadUInt16();
+					ushort difficultyLevel = br.ReadUInt16();
+					ushort activeCivilizations = br.ReadUInt16();
+					ushort currentResearched = br.ReadUInt16();
+					string[] leaderNames = BytesToArray(br.ReadBytes(112), 14);
+					string[] civNames = BytesToArray(br.ReadBytes(96), 12);
+					//
+					
+					string title;
+					string leaderName = leaderNames[humanPlayer];
+					string tribeName = civNames[humanPlayer];
+					switch (difficultyLevel)
+					{
+						case 0: title = "Chief"; break;
+						case 1: title = "Lord"; break;
+						case 2: title = "Prince"; break;
+						case 3: title = "King"; break;
+						default: title = "Emperor"; break;
+					}
+					
+					Name = string.Format("{0} {1}, {2}/{3}", title, leaderName, tribeName, Common.YearString(gameTurn).Replace(".", ""));
+				}
+				ValidFile = true;
+				//TODO: Handle invalid save files
+			}
+		}
+		
 		private readonly Color[] _palette;
 		private char _driveLetter = 'C';
 		private bool _update = true;
 		private Menu _menu;
 		
 		public bool Cancel { get; private set; }
+		
+		private IEnumerable<SaveGameFile> GetSaveGames()
+		{
+			string path = Path.Combine(Settings.Instance.SavesDirectory, _driveLetter.ToString());
+			for (int i = 0; i < 10; i++)
+			{
+				string filename = Path.Combine(path, string.Format("CIVIL{0}", i));
+				yield return new SaveGameFile(filename);
+			}
+		}
 		
 		private void DrawDriveQuestion()
 		{
@@ -95,10 +177,13 @@ namespace CivOne.Screens
 				};
 				
 				Menu.Item menuItem;
-				for (int i = 0; i < 10; i++)
+				
+				int i = 0;
+				foreach (SaveGameFile file in GetSaveGames())
 				{
-					_menu.Items.Add(menuItem = new Menu.Item("(EMPTY)", i));
+					_menu.Items.Add(menuItem = new Menu.Item(file.Name, i++));
 				}
+				Cursor = MouseCursor.Pointer;
 			}
 			else if (c >= 'A' && c <= 'Z')
 			{
@@ -106,6 +191,27 @@ namespace CivOne.Screens
 				_update = true;
 				return true;
 			}
+			return false;
+		}
+		
+		public override bool MouseDown(MouseEventArgs args)
+		{
+			if (_menu != null)
+				return _menu.MouseDown(args);
+			return false;
+		}
+		
+		public override bool MouseUp(MouseEventArgs args)
+		{
+			if (_menu != null)
+				return _menu.MouseUp(args);
+			return false;
+		}
+		
+		public override bool MouseDrag(MouseEventArgs args)
+		{
+			if (_menu != null)
+				return _menu.MouseDrag(args);
 			return false;
 		}
 		
