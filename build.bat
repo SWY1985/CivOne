18dd28a063e6802b02c71da07c8c743de1eb0942
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-06-02 11:01:14 +0200

    Removed ECHO lines

--- build.bat
+++ build.bat
@@ -1,20 +1,12 @@
 @echo off
 
-ECHO.
-ECHO %1
-ECHO %2
-ECHO %3
-ECHO.
-
 IF(%2)==(build) GOTO Build:
 IF(%2)==(run) GOTO Run:
 
 GOTO Error:
 
 :Build
-@echo on
 %1 CivOne.csproj /t:Rebuild /p:Configuration=%3
-@echo off
 GOTO End:
 
 :Run
