493d8d40478360fe0b94b519abbe4ec55d0380b8
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-06-04 06:52:22 +0200

    Added classes for all buildings

---
+++ src/Buildings/Factory.cs
@@ -0,0 +1,22 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Templates;
+
+namespace CivOne.Buildings
+{
+	internal class Factory : BaseBuilding
+	{
+		public Factory() : base(20, 4)
+		{
+			Name = "Factory";
+			RequiredTech = null;
+		}
+	}
+}
