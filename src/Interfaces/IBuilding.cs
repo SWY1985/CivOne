ae30b11405323750216c69abaf04836e4da1ff83
SWY1985 <mail@swouters.nl>
2015-06-04 06:51:34 +0200

    Added interface for Buildings

---
+++ src/Interfaces/IBuilding.cs
@@ -0,0 +1,20 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Enums;
+
+namespace CivOne.Interfaces
+{
+	public interface IBuilding : ICivilopedia
+	{
+		IAdvance RequiredTech { get; }
+		byte Price { get; }
+		byte Maintainance { get; }
+	}
+}
