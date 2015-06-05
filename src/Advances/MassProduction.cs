ab9b9ff0a1c8fd97f9ef166c90899872c196a4db
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2015-06-05 16:23:46 +0200

    Added classes for all advances

---
+++ src/Advances/MassProduction.cs
@@ -0,0 +1,23 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Enums;
+using CivOne.Templates;
+
+namespace CivOne.Advances
+{
+	internal class MassProduction : BaseAdvance
+	{
+		public MassProduction() : base(Advance.Automobile, Advance.TheCorporation)
+		{
+			Name = "Mass Production";
+			Type = Advance.MassProduction;
+		}
+	}
+}
---
+++ src/Advances/TheCorporation.cs
@@ -0,0 +1,23 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Enums;
+using CivOne.Templates;
+
+namespace CivOne.Advances
+{
+	internal class TheCorporation : BaseAdvance
+	{
+		public TheCorporation() : base(Advance.Banking, Advance.Industrialization)
+		{
+			Name = "The Corporation";
+			Type = Advance.TheCorporation;
+		}
+	}
+}
