fff2f4b77b7cb8d0f6f537bcb6dff9aa5e4a3fc6
SWY1985 <mail@swouters.nl>
2015-06-05 16:23:46 +0200

    Added classes for all advances

---
+++ src/Advances/Masonry.cs
@@ -0,0 +1,23 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Enums;
+using CivOne.Templates;
+
+namespace CivOne.Advances
+{
+	internal class Masonry : BaseAdvance
+	{
+		public Masonry() : base()
+		{
+			Name = "Masonry";
+			Type = Advance.Masonry;
+		}
+	}
+}
