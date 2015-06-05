9deba2c8b86feee6f5ff25cad7ab7d7bd5ef2519
SWY1985 <mail@swouters.nl>
2015-06-05 16:23:07 +0200

    Added Advance enum

---
+++ src/Enums/Advance.cs
@@ -0,0 +1,82 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+namespace CivOne.Enums
+{
+	internal enum Advance
+	{
+		Alphabet,
+		CodeOfLaws,
+		Currency,
+		AtomicTheory,
+		Democracy,
+		Monarchy,
+		Astronomy,
+		MapMaking,
+		Navigation,
+		Mathematics,
+		Medicine,
+		Physics,
+		Engineering,
+		University,
+		Magnetism,
+		Electronics,
+		Masonry,
+		BronzeWorking,
+		IronWorking,
+		BridgeBuilding,
+		Invention,
+		Computers,
+		Writing,
+		SteamEngine,
+		Trade,
+		CeremonialBurial,
+		Mysticism,
+		NuclearFission,
+		Philosophy,
+		Religion,
+		Literacy,
+		HorsebackRiding,
+		Feudalism,
+		TheWheel,
+		Gunpowder,
+		Industrialization,
+		Chemistry,
+		Combustion,
+		Flight,
+		AdvancedFlight,
+		SpaceFlight,
+		MassProduction,
+		Pottery,
+		Communism,
+		TheRepublic,
+		Construction,
+		Rocketry,
+		TheCorporation,
+		Metallurgy,
+		RailRoad,
+		NuclearPower,
+		TheoryOfGravity,
+		Steel,
+		Banking,
+		Electricity,
+		Refining,
+		Explosives,
+		SuperConductor,
+		Automobile,
+		GeneticEngineering,
+		Plastics,
+		Recycling,
+		Chivalry,
+		Robotics,
+		Conscription,
+		LaborUnion,
+		FusionPower
+	}
+}
