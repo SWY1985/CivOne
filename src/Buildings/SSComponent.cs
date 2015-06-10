3462919297171a6a2a15c563228ce35ed5b3ba12
SWY1985 <mail@swouters.nl>
2015-06-10 16:35:10 +0200

    Added Space Ship parts

---
+++ src/Buildings/SSComponent.cs
@@ -0,0 +1,25 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Advances;
+using CivOne.Enums;
+using CivOne.Templates;
+
+namespace CivOne.Buildings
+{
+	internal class SSComponent : BaseBuilding
+	{
+		public SSComponent() : base(16)
+		{
+			Name = "SS Component";
+			RequiredTech = new Plastics();
+			Type = Building.SSComponent;
+		}
+	}
+}
---
+++ src/Buildings/SSStructural.cs
@@ -0,0 +1,25 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using CivOne.Advances;
+using CivOne.Enums;
+using CivOne.Templates;
+
+namespace CivOne.Buildings
+{
+	internal class SSStructural : BaseBuilding
+	{
+		public SSStructural() : base(8)
+		{
+			Name = "SS Structural";
+			RequiredTech = new SpaceFlight();
+			Type = Building.SSStructural;
+		}
+	}
+}
