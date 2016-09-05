a2d5bb3b4ebab64e46ffe7da5f05420db0aeb8fe
SWY1985 <mail@swouters.nl>
2016-09-05 16:27:19 +0200

    Draw city resources, handle shields, closes #37
    Also partially implements #38 and #39.

--- src/Screens/CityResources.cs
+++ src/Screens/CityResources.cs
@@ -8,6 +8,7 @@
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System.Drawing;
+using System.Linq;
 using CivOne.Enums;
 using CivOne.GFX;
 using CivOne.Interfaces;
@@ -32,6 +33,13 @@ namespace CivOne.Screens
 				_canvas.FillRectangle(1, 1, 1, 121, 8);
 				_canvas.FillRectangle(0, 123, 0, 1, 43);
 				_canvas.DrawText($"City Resources", 1, 17, 6, 2, TextAlign.Left);
+
+				for (int i = 0; i < _city.ResourceTiles.Sum(t => t.Food); i++)
+					AddLayer(Icons.Food, 1 + (8 * i), 9);
+				for (int i = 0; i < _city.ResourceTiles.Sum(t => t.Shield); i++)
+					AddLayer(Icons.Shield, 1 + (8 * i), 17);
+				for (int i = 0; i < _city.ResourceTiles.Sum(t => t.Trade); i++)
+					AddLayer(Icons.Trade, 1 + (8 * i), 25);
 				
 				_update = false;
 			}
