a162633065ea8cc19eeee040b8057f2f7d929ea4
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2016-09-15 11:38:05 +0200

    Trireme can get lost at sea
    This closes #85

--- src/Units/Trireme.cs
+++ src/Units/Trireme.cs
@@ -7,15 +7,35 @@
 // You should have received a copy of the CC0 legalcode along with this
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+using System.Linq;
 using CivOne.Advances;
 using CivOne.Enums;
 using CivOne.Interfaces;
+using CivOne.IO;
+using CivOne.Tasks;
 using CivOne.Templates;
+using CivOne.Tiles;
 
 namespace CivOne.Units
 {
 	internal class Trireme : BaseUnitSea, IBoardable
 	{
+		protected override void MovementDone(ITile previousTile)
+		{
+			base.MovementDone(previousTile);
+			
+			if (MovesLeft > 0) return;
+
+			// Check if the Trireme is at open sea
+			if (Tile.GetBorderTiles().Any(t => !(t is Ocean))) return;
+
+			// The Trireme unit is surrounded by oceans, there's a 50% chance it will be lost at sea
+			if (Common.Random.Next(0, 100) < 50) return;
+
+			Game.Instance.DisbandUnit(this);
+			GameTask.Enqueue(Message.Error("-- Civilization Note --", TextFile.Instance.GetGameText("ERROR/TRIREME")));
+		}
+
 		public int Cargo
 		{
 			get
