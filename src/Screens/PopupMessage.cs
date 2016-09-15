e865d539312ba46c1625b708a5a7a0162e3dc804
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2016-09-15 11:04:24 +0200

    Implemented Notice and Warning popups
    This closes #131

---
+++ src/Screens/PopupMessage.cs
@@ -0,0 +1,74 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System;
+using System.Drawing;
+using System.Linq;
+using CivOne.Enums;
+using CivOne.Events;
+using CivOne.GFX;
+//using CivOne.Interfaces;
+using CivOne.Templates;
+
+namespace CivOne.Screens
+{
+	internal class PopupMessage : BaseScreen
+	{
+		private bool _update = true;
+		
+		public override bool HasUpdate(uint gameTick)
+		{
+			if (_update)
+			{
+				_update = false;
+				return true;
+			}
+			return false;
+		}
+		
+		public override bool KeyDown(KeyboardEventArgs args)
+		{
+			HandleClose();
+			Destroy();
+			return true;
+		}
+		
+		public override bool MouseDown(ScreenEventArgs args)
+		{
+			HandleClose();
+			Destroy();
+			return true;
+		}
+
+		public PopupMessage(byte colour, string title, string[] message)
+		{
+			Cursor = MouseCursor.Pointer;
+			
+			Color[] palette = Resources.Instance.LoadPIC("SP257").Image.Palette.Entries;
+			_canvas = new Picture(320, 200, palette);
+			
+			byte colourLight = (byte)(colour + 8);
+			int lineHeight = Resources.Instance.GetFontHeight(1);
+			int width = 209;
+			int height = ((message.Length + (title != null ? 1 : 0)) * lineHeight) + 5;
+
+			_canvas.FillRectangle(colourLight, 56, 16, width, 1);
+			_canvas.FillRectangle(colourLight, 56, 17, 1, height - 2);
+			_canvas.FillRectangle(colourLight, 56 + width - 1, 17, 1, height - 2);
+			_canvas.FillRectangle(colourLight, 56, 16 + height - 1, width, 1);
+			_canvas.FillRectangle(colour, 57, 17, width - 2, height - 2);
+
+			int yy = 19 - lineHeight;
+			if (title != null)
+				_canvas.DrawText(title, 1, 5, 160, (yy += lineHeight), TextAlign.Center);
+			for (int i = 0; i < message.Length; i++)
+				_canvas.DrawText(message[i], 1, 15, 64, (yy += lineHeight));
+		}
+	}
+}
--- src/Tasks/Message.cs
+++ src/Tasks/Message.cs
@@ -17,11 +17,6 @@ namespace CivOne.Tasks
 	internal class Message : GameTask
 	{
 		private readonly IScreen _screen;
-		/*
-		private readonly string _title;
-		private readonly string[] _message;
-		private readonly bool _newspaper;
-		private bool _showGovernment;*/
 
 		private void ClosedMessage(object sender, EventArgs args)
 		{
@@ -30,48 +25,38 @@ namespace CivOne.Tasks
 
 		public override void Run()
 		{
-			//TribalHut tribalHut = new TribalHut(_message);
-			//tribalHut.Closed += ClosedMessage;
-			//Common.AddScreen(tribalHut);
 			_screen.Closed += ClosedMessage;
 			Common.AddScreen(_screen);
 		}
-/*
-		private static Message Newspaper(bool goverment, string city, string[] message)
-		{
-			return new Message(city, message, newspaper: true)
-			{
-				_showGovernment = 
-			};
-		}*/
 
 		public static Message NewGoverment(City city, params string[] message)
 		{
 			return new Message(new Newspaper(city, message, showGovernment: true));
-			//return Newspaper(true, city, message);
 		}
 
 		public static Message Newspaper(City city, params string[] message)
 		{
 			return new Message(new Newspaper(city, message, showGovernment: false));
-			//return Newspaper(false, city, message);
 		}
 
 		public static Message TribalHut(params string[] message)
 		{
-			//return new Message(null, message);
 			return new Message(new TribalHut(message));
+		}
+
+		public static Message Notice(string title, params string[] message)
+		{
+			return new Message(new PopupMessage(2, title, message));
+		}
+
+		public static Message Warning(string title, params string[] message)
+		{
+			return new Message(new PopupMessage(4, title, message));
 		}
 
 		private Message(IScreen screen)
 		{
 			_screen = screen;
 		}
-/*
-		private Message(string title, string[] message, bool newspaper = false)
-		{
-			_title = title;
-			_message = message;
-		}*/
 	}
 }
