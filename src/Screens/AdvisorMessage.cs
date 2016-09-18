01c7fc8b6ed4163e7911fef830d0b0ffeb85dff5
SWY1985 <mail@swouters.nl>
2016-09-18 08:16:53 +0200

    Removed commented out code

--- src/Screens/AdvisorMessage.cs
+++ src/Screens/AdvisorMessage.cs
@@ -89,26 +89,6 @@ namespace CivOne.Screens
 				left = 58;
 			_canvas.FillRectangle(5, left, 72, actualWidth + 2, height + 2);
 			AddLayer(messageBox, left + 1, 73);
-			
-			/*
-			Bitmap[] textLines = new Bitmap[message.Length];
-			for (int i = 0; i < message.Length; i++)
-				textLines[i] = Resources.Instance.GetText(message[i], 0, 15);
-			int width = textLines.Max(x => x.Width) + 10;
-			int height = textLines.Sum(x => x.Height) + 9;
-
-			int fillWidth = 4 - (width % 4); 
-			width += fillWidth;
-			int actualWidth = width - fillWidth;
-
-			_messageBox = new Picture(width, height);
-			_messageBox.FillLayerTile(background, 1, 1);
-			if (fillWidth > 0)
-				_messageBox.FillRectangle(0, actualWidth, 0, fillWidth, height);
-			_messageBox.AddBorder(15, 8, 1, 1, actualWidth - 2, height - 2);
-			_messageBox.AddBorder(5, 5, 0, 0, actualWidth, height);
-			for (int i = 0; i < textLines.Length; i++)
-				_messageBox.AddLayer(textLines[i], 5, (textLines[i].Height * i) + 5);*/
 		}
 	}
 }
