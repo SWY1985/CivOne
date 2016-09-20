92748d2cb86bc51f4971f778042a59fc298e69e6
SWY1985 <mail@swouters.nl>
2016-09-20 15:17:18 +0200

    Implemented Barracks effect

--- src/City.cs
+++ src/City.cs
@@ -414,6 +414,7 @@ namespace CivOne
 					Shields = 0;
 					IUnit unit = Game.Instance.CreateUnit((CurrentProduction as IUnit).Type, X, Y, Owner);
 					unit.SetHome(this);
+					unit.Veteran = (_buildings.Any(b => (b is Barracks)));
 					if ((unit is Settlers) || (unit is Diplomat) || (unit is Caravan))
 					{
 						AdvisorMessage advisorMessage = new AdvisorMessage(Advisor.Defense, $"{this.Name} builds {unit.Name}.");
