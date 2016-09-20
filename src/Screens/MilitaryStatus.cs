0894d97edecbd3a08df36d302d8e1844409f33b5
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2016-09-20 21:48:15 +0200

    #133 Fully implemented Military Status (F2) screen

--- src/Screens/MilitaryStatus.cs
+++ src/Screens/MilitaryStatus.cs
@@ -7,6 +7,10 @@
 // You should have received a copy of the CC0 legalcode along with this
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+using System.Linq;
+using CivOne.Enums;
+using CivOne.GFX;
+using CivOne.Interfaces;
 using CivOne.Templates;
 
 namespace CivOne.Screens
@@ -15,7 +19,32 @@ namespace CivOne.Screens
 	{
 		public MilitaryStatus() : base("MILITARY STATUS", 1)
 		{
+			byte player = Game.Instance.PlayerNumber(HumanPlayer);
+			IUnit[] units = Game.Instance.GetUnits().Where(u => u.Owner == player && u.Home != null).ToArray();
+			IUnit[] production = Game.Instance.GetCities().Where(c => c.Owner == player).Where(c => (c.CurrentProduction is IUnit)).Select(c => (c.CurrentProduction as IUnit)).ToArray();
+
+			int i = 0;
+			foreach (IUnit unit in Reflect.GetUnits())
+			{
+				if (!units.Any(u => u.Type == unit.Type) && !production.Any(u => u.Type == unit.Type)) continue;
+
+				int active = units.Count(u => u.Type == unit.Type);
+				int inProduction = production.Count(u => u.Type == unit.Type);
+
+				Picture icon = unit.GetUnit(player, false);
+				AddLayer(icon, ((i % 2 == 0) ? 1 : 18), 27 + (9 * i));
+				_canvas.FillRectangle(9, 36, 30 + (i * 9), 284, 1);
+				_canvas.DrawText(unit.Name, 0, 15, 36, 32 + (i * 9));
+				_canvas.DrawText($"({unit.Attack}/{unit.Defense}/{unit.Move})", 0, 11, 112, 32 + (i * 9));
+				if (active > 0)
+					_canvas.DrawText($"{active} active", 0, 15, 168, 32 + (i * 9));
+				if (inProduction > 0)
+					_canvas.DrawText($"{inProduction} in production", 0, 11, 232, 32 + (i * 9));
+				
+				i++;
+			}
 			
+			AddLayer(Portrait[(int)Advisor.Defense], 278, 2);
 		}
 	}
 }
