c70680b50771aed9c6b3ea55008b17a689d73be4
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2016-09-26 13:42:24 +0200

    Cancel dialog after selecting quit
    Just to be sure...

--- src/Screens/Dialogs/ConfirmQuit.cs
+++ src/Screens/Dialogs/ConfirmQuit.cs
@@ -7,12 +7,19 @@
 // You should have received a copy of the CC0 legalcode along with this
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+using System;
 using CivOne.Templates;
 
 namespace CivOne.Screens.Dialogs
 {
 	internal class ConfirmQuit : BaseDialog
 	{
+		private void MenuQuit(object sender, EventArgs args)
+		{
+			Common.Quit();
+			Cancel();
+		}
+
 		protected override void FirstUpdate()
 		{
 			Menu menu = new Menu(Canvas.Image.Palette.Entries, Selection(3, 20, 100, 16))
@@ -24,14 +31,13 @@ namespace CivOne.Screens.Dialogs
 				TextColour = 5,
 				FontId = 0
 			};
-			Menu.Item menuItem;
 			int i = 0;
 			foreach (string choice in new [] { "Keep Playing", "Yes, Quit" })
 			{
 				menu.Items.Add(new Menu.Item(choice, i++));
 			}
 			menu.Items[0].Selected += Cancel;
-			menu.Items[1].Selected += (s, a) => Common.Quit();
+			menu.Items[1].Selected += MenuQuit;
 
 			menu.MissClick += Cancel;
 			menu.Cancel += Cancel;
