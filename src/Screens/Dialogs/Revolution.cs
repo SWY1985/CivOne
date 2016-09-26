f91329b52bddc912b316b39c773ca5523f48c091
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2016-09-26 13:42:42 +0200

    Revolution choice menu is now a task/dialog

---
+++ src/Screens/Dialogs/Revolution.cs
@@ -0,0 +1,54 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System;
+using CivOne.Templates;
+
+namespace CivOne.Screens.Dialogs
+{
+	internal class Revolution : BaseDialog
+	{
+		private void MenuRevolution(object sender, EventArgs args)
+		{
+			Game.HumanPlayer.Revolt();
+			Cancel();
+		}
+
+		protected override void FirstUpdate()
+		{
+			Menu menu = new Menu(Canvas.Image.Palette.Entries, Selection(3, 12, 228, 16))
+			{
+				X = 66,
+				Y = 91,
+				Width = 227,
+				ActiveColour = 11,
+				TextColour = 5,
+				FontId = 0,
+				Indent = 2
+			};
+			int i = 0;
+			foreach (string choice in new [] { "_No, thanks.", "_Yes, we need a new government." })
+			{
+				menu.Items.Add(new Menu.Item(choice, i++));
+			}
+			menu.Items[0].Selected += Cancel;
+			menu.Items[1].Selected += MenuRevolution;
+
+			menu.MissClick += Cancel;
+			menu.Cancel += Cancel;
+			Menus.Add(menu);
+			Common.AddScreen(menu);
+		}
+
+		public Revolution() : base(63, 79, 234, 33)
+		{
+			DialogBox.DrawText("Are you sure you want a REVOLUTION?", 0, 15, 5, 5);
+		}
+	}
+}
