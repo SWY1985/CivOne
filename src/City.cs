9db8b029168ba463394f3f1eaf70e56196e3b8e2
SWY1985 <mail@swouters.nl>
2016-09-30 08:30:05 +0200

    Implemented temporary production costs calculation
    #203

--- src/City.cs
+++ src/City.cs
@@ -342,7 +342,13 @@ namespace CivOne
 		{
 			get
 			{
-				// TODO: Calculate buy price
+				if (Shields > 0)
+				{
+					double totalPrice = (double)CurrentProduction.BuyPrice / 2;
+					double progressLeft = 1 - (double)(1.0 / (CurrentProduction.Price * 10)) * Shields;
+
+					return (short)(Math.Floor(totalPrice * progressLeft));
+				}
 				return CurrentProduction.BuyPrice;
 			}
 		}
