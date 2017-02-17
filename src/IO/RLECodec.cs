ccf66bae1f1458b7629d4ca018cfda44143a1c90
SWY1985 <mail@swouters.nl>
2017-02-17 13:14:36 +0100

    Implemented RLE encoder

--- src/IO/RLECodec.cs
+++ src/IO/RLECodec.cs
@@ -18,6 +18,7 @@ namespace CivOne.IO
 	internal class RLECodec
 	{
 		private const int RLE_REPEAT = 0x90;
+		private const int RLE_ESCAPE = 0x00;
 
 		public static byte[] Decode(byte[] input)
 		{
@@ -26,19 +27,45 @@ namespace CivOne.IO
 				byte value = input[0];
 				for (int i = 0; i < input.Length; i++)
 				{
-					if (input[i] != RLE_REPEAT || input[i + 1] == 0)
+					if (input[i] != RLE_REPEAT || input[i + 1] == RLE_ESCAPE)
 					{
 						value = input[i];
 						ms.WriteByte(value);
-						if (input[i] == RLE_REPEAT && input[i + 1] == 0) i++;
+						if (input[i] == RLE_REPEAT && input[i + 1] == RLE_ESCAPE) i++;
 						continue;
 					}
 
 					int repeat = input[i + 1];
-
 					ms.Write(Enumerable.Repeat(value, repeat).ToArray(), 0, repeat - 1);
 					i++;
 				}
+				return ms.ToArray();
+			}
+		}
+
+		public static byte[] Encode(byte[] input)
+		{
+			using (MemoryStream ms = new MemoryStream())
+			{
+				for (int i = 0; i < input.Length; i++)
+				{
+					byte value = input[i];
+					byte repeat = 1;
+					for (int r = 1; r < 255 && (i + r) < input.Length; r++)
+					{
+						if (input[i + r] != value) break;
+						repeat++;
+					}
+
+					ms.WriteByte(value);
+					if (value == RLE_REPEAT) ms.WriteByte(RLE_ESCAPE);
+					if (repeat == 1) continue;
+					ms.WriteByte(RLE_REPEAT);
+					ms.WriteByte(repeat);
+					if (repeat == RLE_REPEAT) ms.WriteByte(RLE_ESCAPE);
+					i += (repeat - 1);
+				}
+
 				return ms.ToArray();
 			}
 		}
