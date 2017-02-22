6e89754e14dea63b9066d2dd9de01d1b4fa33176
SWY1985 <mail@swouters.nl>
2017-02-22 21:23:19 +0100

    #32 Reviewed city drawing algorithm, added building
    New buildings: Library, Courthouse, Bank, Cathedral, University and
    Colosseum. Also, roads are now generated horizontally and vertically.

---
+++ src/Enums/CityViewMap.cs
@@ -0,0 +1,30 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+namespace CivOne.Enums
+{
+	internal enum CityViewMap
+	{
+		Empty = 0,
+		Road = 1,
+		House = 2,
+		Tree = 3,
+		Barracks,
+		Granary,
+		Temple,
+		MarketPlace,
+		Library,
+		Courthouse,
+		Bank,
+		Cathedral,
+		University,
+		Colosseum,
+		Occupied = 255
+	}
+}
