3708875fb78ffe1d303e0df316c2441dd296a837
SWY1985 <mail@swouters.nl>
2017-02-24 15:37:35 +0100

    #218 Implemented Unit combat (unfinished)
    Units can win or lose when attacking. Doesn't work (correctly) on cities
    and unit stacks.

--- src/Templates/BaseUnit.cs
+++ src/Templates/BaseUnit.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using CivOne.Buildings;
 using CivOne.Enums;
 using CivOne.GFX;
 using CivOne.Interfaces;
@@ -84,6 +85,143 @@ namespace CivOne.Templates
 		}
 		public MoveUnit Movement { get; private set; }
 
+		private int AttackStrength(IUnit defendUnit)
+		{
+			// Step 1: Determine the nominal attack value of the attacking unit and multiply it by 8.
+			int attackStrength = ((int)Attack * 8);
+
+			if (Owner == 0)
+			{
+				// Step 2: If the attacking unit is a Barbarian unit and the defending unit is player-controlled, multiply the attack strength by the Difficulty Modifier, then divide it by 4.
+				if (defendUnit.Owner == Game.PlayerNumber(Human))
+				{
+					attackStrength *= (Game.Difficulty + 1);
+					attackStrength /= 4;
+				}
+
+				// Step 3: If the attacking unit is a Barbarian unit and the defensing unit is AI-controlled, divide the attack strength by 2.
+				if (defendUnit.Owner != Game.PlayerNumber(Human))
+				{
+					attackStrength /= 2;
+				}
+
+				// Step 4: If the attacking unit is a Barbarian unit and the defending unit is inside a city and the defending civilization does not control any other cities, set the attack strength to zero.
+				// This actually makes the defending unit invincible in this special case. Might well save you from being obliterated by that unlucky hut at 3600BC.
+				if (defendUnit.Tile.City != null && Game.GetPlayer(defendUnit.Owner).Cities.Length == 1)
+				{
+					attackStrength = 0;
+				}
+
+				// Step 5: If the attacking unit is a Barbarian unit and the defending unit is inside a city with a Palace, divide the attack strength by 2.
+				if (defendUnit.Tile.City != null && defendUnit.Tile.City.HasBuilding<Palace>())
+				{
+					attackStrength /= 2;
+				}
+			}
+
+			// Step 6: If the attacking unit is a veteran unit, increase the attack strength by 50%.
+			if (Veteran)
+			{
+				attackStrength *= 2;
+			}
+			
+			// Step 7: If the attacking unit has only 0.2 movement points left, multiply the attack strength by 2, then divide it by 3. If the attacking unit has only 0.1 movement points left, then just divide by 3 instead.
+			if (MovesLeft == 0)
+			{
+				attackStrength *= PartMoves;
+				attackStrength /= PartMoves;
+			}
+
+			// Step 8: If the attacking unit is a Barbarian unit and the defending unit is player-controlled, check the difficulty level. On Chieftain and Warlord levels, divide the attack strength by 2.
+			if (Owner == 0 && defendUnit.Owner == Game.PlayerNumber(Human))
+			{
+				if (Game.Difficulty < 2)
+				{
+					attackStrength /= 2;
+				}
+			}
+
+			// Step 9: If the attacking unit is player-controlled, check the difficulty level. On Chieftain level, multiply the attack strength by 2.
+			// So on Chieftain difficulty, it is often better to attack than be attacked, even with a defensive unit.
+			if (Owner == Game.PlayerNumber(Human) && Game.Difficulty == 0)
+			{
+				attackStrength *= 2;
+			}
+
+			return attackStrength;
+		}
+
+		private int DefendStrength(IUnit defendUnit, IUnit attackUnit)
+		{
+			// Check City Walls for step 5
+			bool cityWalls = (defendUnit.Tile.City != null && defendUnit.Tile.City.HasBuilding<CityWalls>());
+
+			// Step 1: Determine the nominal defense value of defending unit.
+			int defendStrength = (int)defendUnit.Defense;
+
+			if (defendUnit.Class == UnitClass.Land || (defendUnit.Class == UnitClass.Water && cityWalls && attackUnit.Attack != 12))
+			{
+				int fortificationModifier = 4;
+				if (defendUnit.Tile.Fortress)
+					fortificationModifier = 8;
+				else if (defendUnit.Fortify || defendUnit.FortifyActive)
+					fortificationModifier = 6;
+
+				// Step 2: If the defending unit is a ground unit, multiply the defense strength by the Terrain Modifier.
+				// This modifier effectively includes a factor of 2.
+				defendStrength *= defendUnit.Tile.Defense;
+				
+				if (!cityWalls || attackUnit.Attack == 12)
+				{
+					// Step 3: If the defending unit is a ground unit, multiply the defense strength by the Fortification Modifier.
+					// This modifier effectively includes a factor of 4, resulting in a combined factor of 8.
+					defendStrength *= fortificationModifier;
+				}
+			}
+
+			// Step 4: If the defending unit is a sea or air unit, multiply the defense strength by 8.
+			// This effectively treats the Terrain Modifier as 2, regardless of the actual terrain type. It also means that these units will never benefit from the Fortification Modifier.
+			if (defendUnit.Class != UnitClass.Land && (!cityWalls || attackUnit.Attack == 12))
+			{
+				defendStrength *= 8;
+			}
+
+			// Step 5: If the defending unit is inside a city with City Walls and the nominal attack value of the attacking unit is NOT equal to 12, check the domain of the defending unit. If the domain is NOT air, re-calculate steps 1 and 2 (ignore steps 3 and 4) and multiply the result by 12.
+			// When determining if the attacking unit ignores City Walls, the game just checks for attack value, not unit type. So if you change any unit's attack rating to 12, the game will have it ignore City Walls as well.
+			if (cityWalls && attackUnit.Attack != 12)
+			{
+				defendStrength *= 12;
+			}
+
+			// Step 6: If the defending unit is a veteran unit, increase the defense strength by 50%.
+			if (defendUnit.Veteran)
+			{
+				defendStrength += (defendStrength / 2);
+			}
+
+			return defendStrength;
+		}
+
+		private bool AttackOutcome(IUnit attackUnit, ITile defendTile)
+		{
+			IUnit defendUnit = defendTile.Units.OrderByDescending(x => x.Attack * (x.Veteran ? 1.5 : 1)).ThenBy(x => (int)x.Type).First();
+
+			int attackStrength = AttackStrength(defendUnit);
+			int defenseStrength = DefendStrength(defendUnit, attackUnit);
+			int randomAttack = Common.Random.Next(attackStrength);
+			int randomDefense = Common.Random.Next(defenseStrength);
+			bool win = (randomAttack > randomDefense);
+			if (win && attackUnit.Owner == 0 && defendUnit.Tile.City != null)
+			{
+				 // If the attacking unit is a Barbarian unit and the defending unit is inside a city, then, if the attacking unit won, the procedure will be repeated once
+				 // This time, the attacking unit wins on a tie.
+				randomAttack = Common.Random.Next(attackStrength);
+				randomDefense = Common.Random.Next(defenseStrength);
+				win = (randomAttack >= randomDefense);
+			}
+			return win;
+		}
+
 		protected virtual bool Confront(int relX, int relY)
 		{
 			if (Class == UnitClass.Land && (this is Diplomat || this is Caravan))
@@ -93,7 +231,26 @@ namespace CivOne.Templates
 			}
 
 			Movement = new MoveUnit(relX, relY);
-			Movement.Done += (s, a) => GameTask.Insert(Show.DestroyUnit(this));
+			if (AttackOutcome(this, Map[X, Y][relX, relY]))
+			{
+				Movement.Done += (s, a) =>
+				{
+					GameTask.Insert(Show.DestroyUnit(Map[X, Y][relX, relY].Units.First()));
+					if (MovesLeft == 0)
+						PartMoves = 0;
+					else if (MovesLeft > 0)
+						MovesLeft--;
+					Movement = null;
+				};
+			}
+			else
+			{
+				Movement.Done += (s, a) =>
+				{
+					GameTask.Insert(Show.DestroyUnit(this));
+					Movement = null;
+				};
+			}
 			GameTask.Insert(Movement);
 			return false;
 		}
@@ -106,7 +263,8 @@ namespace CivOne.Templates
 			if (moveTarget == null) return false;
 			if (moveTarget.Units.Any(u => u.Owner != Owner))
 			{
-				return Confront(relX, relY);
+				Confront(relX, relY);
+				return true;
 			}
 			if (Class == UnitClass.Land && !(this is Diplomat || this is Caravan) && !new ITile[] { Map[X, Y], moveTarget }.Any(t => t.IsOcean || t.City != null) && moveTarget.GetBorderTiles().SelectMany(t => t.Units).Any(u => u.Owner != Owner))
 			{
