6508607bb7098809ffb613b204459f6178abe17f
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2017-02-24 10:31:59 +0100

    Removed unnecessary usings

--- src/Screens/King.cs
+++ src/Screens/King.cs
@@ -8,7 +8,6 @@
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using CivOne.Advances;
-using CivOne.Civilizations;
 using CivOne.Events;
 using CivOne.GFX;
 using CivOne.Governments;
