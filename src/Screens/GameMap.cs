6f388ace08d9e2e757a39373d98ac94004d4cb63
SWY1985 <mail@swouters.nl>
2017-03-01 15:02:14 +0100

    Do not center on city or unit when clicking on them
    This closes #210

--- src/Screens/GameMap.cs
+++ src/Screens/GameMap.cs
@@ -463,24 +463,23 @@ namespace CivOne.Screens
 			}
 			if ((args.Buttons & MouseButton.Left) > 0)
 			{
-				_x += x - 8;
-				_y += y - 6;
-				while (_x < 0) _x += Map.WIDTH;
-				while (_x >= Map.WIDTH) _x -= Map.WIDTH;
-				while (_y < 0) _y++;
-				while (_y + _tilesY > Map.HEIGHT) _y--;
-				_update = true;
-				
-				if (city != null)
+				if (city != null && city.Owner == Game.PlayerNumber(Human) || Settings.RevealWorld)
 				{
-					if (city.Owner == Game.PlayerNumber(Human) || Settings.RevealWorld)
-					{
-						Common.AddScreen(new CityManager(city));
-					}
+					Common.AddScreen(new CityManager(city));
 				}
 				else if (Map[xx, yy].Units.Any(u => u.Owner == Game.PlayerNumber(Human)))
 				{
 					GameTask.Enqueue(Show.UnitStack(xx, yy));
+				}
+				else
+				{
+					_x += x - 8;
+					_y += y - 6;
+					while (_x < 0) _x += Map.WIDTH;
+					while (_x >= Map.WIDTH) _x -= Map.WIDTH;
+					while (_y < 0) _y++;
+					while (_y + _tilesY > Map.HEIGHT) _y--;
+					_update = true;
 				}
 			}
 			return _update;
