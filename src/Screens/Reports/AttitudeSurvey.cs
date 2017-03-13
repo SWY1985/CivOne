09dd005d7152b4c91f10d976f097b8955a341ebe
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2017-03-13 13:14:10 +0100

    Re-fixed number formatting

--- src/Screens/Reports/AttitudeSurvey.cs
+++ src/Screens/Reports/AttitudeSurvey.cs
@@ -82,7 +82,7 @@ namespace CivOne.Screens.Reports
 			y += 8;
 			if (y <= 190)
 			{
-				string population = string.Format("{0:n0}", Human.Population, new CultureInfo("en-US"));
+				string population = Human.Population.ToString("n0", new CultureInfo("en-US"));
 				if (Human.Population == 0) population = "00,000";
 				int happy = 0;
 				int content = 100;
