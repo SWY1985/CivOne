8e6d9c401d367417744e5721b384083d7392a9a2
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2017-03-14 11:07:17 +0100

    Merged BaseScreen Borders and Canvas

--- src/Templates/BaseScreen.Canvas.cs
+++ src/Templates/BaseScreen.Canvas.cs
@@ -35,6 +35,34 @@ namespace CivOne.Templates
 			_canvas.AddLayer(picture, x, y);
 		}
 
+		protected void DrawBorder(int border)
+		{
+			border = (border % 2);
+			Picture[] borders = new Picture[8];
+			int index = 0;
+			for (int yy = 0; yy < 2; yy++)
+			for (int xx = 0; xx < 4; xx++)
+			{
+				borders[index] = Resources.Instance.GetPart("SP299", ((border == 0) ? 192 : 224) + (8 * xx), 120 + (8 * yy), 8, 8);
+				index++;
+			}
+			
+			for (int x = 8; x < 312; x += 8)
+			{
+				AddLayer(borders[4], x, 0);
+				AddLayer(borders[6], x, 192);
+			}
+			for (int y = 8; y < 192; y += 8)
+			{
+				AddLayer(borders[7], 0, y);
+				AddLayer(borders[5], 312, y);
+			}
+			AddLayer(borders[0], 0, 0);
+			AddLayer(borders[1], 312, 0);
+			AddLayer(borders[2], 0, 192);
+			AddLayer(borders[3], 312, 192);
+		}
+
 		protected void DrawButton(string text, byte colour, byte colourDark, int x, int y, int width)
 		{
 			_canvas.FillRectangle(7, x, y, width, 1);
