a52fe1ce7719315ebeb2463b8d279dc58e5f7f1b
SWY1985 <mail@swouters.nl>
2017-07-05 15:31:45 +0200

    Implemented Free unit graphics

--- src/GFX/Free.cs
+++ src/GFX/Free.cs
@@ -27,6 +27,30 @@ namespace CivOne.GFX
 			}
 		}
 
+		private IEnumerable<byte> GenerateUnit()
+		{
+			for (int yy = 0; yy < 16; yy++)
+			for (int xx = 0; xx < 16; xx++)
+			{
+				if ((xx == 0 || xx == 15 || yy == 0 || yy == 15) || ((xx == 1 || xx == 14) && (yy == 1 || yy == 14)))
+				{
+					yield return 0;
+				}
+				else if (xx == 1 || yy == 14)
+				{
+					yield return 15;
+				}
+				else if (xx == 14 || yy == 1)
+				{
+					yield return 2;
+				}
+				else
+				{
+					yield return 10;
+				}
+			}
+		}
+
 		public Picture PanelGrey
 		{
 			get
@@ -57,7 +81,7 @@ namespace CivOne.GFX
 			{
 				if (_landBase == null)
 				{
-					_landBase = new Picture(16, 16, GenerateNoise(35, 36, 37).Take(16 * 16).ToArray(), Common.GetPalette256);
+					_landBase = new Picture(16, 16, GenerateNoise(37, 38, 39).Take(16 * 16).ToArray(), Common.GetPalette256);
 				}
 				return _landBase;
 			}
@@ -69,7 +93,7 @@ namespace CivOne.GFX
 			{
 				if (_seaBase == null)
 				{
-					_seaBase = new Picture(16, 16, GenerateNoise(72, 73, 74).Take(16 * 16).ToArray(), Common.GetPalette256);
+					_seaBase = new Picture(16, 16, GenerateNoise(77, 78, 79).Take(16 * 16).ToArray(), Common.GetPalette256);
 				}
 				return _seaBase;
 			}
@@ -93,6 +117,46 @@ namespace CivOne.GFX
 					output.AddLayer(new Picture(16, 3, GenerateNoise(28, 29, 30, 0, 31).Take(16 * 3).ToArray(), Common.GetPalette256), 0, 0);
 					break;
 			}
+			return output;
+		}
+
+		public Picture GetUnit(Unit type)
+		{
+			Picture output = new Picture(16, 16, GenerateUnit().ToArray(), Common.GetPalette256);
+			char text = ' ';
+			switch (type)
+			{
+				case Unit.Settlers: text = 'S'; break;
+				case Unit.Militia: text = 'm'; break;
+				case Unit.Phalanx: text = 'P'; break;
+				case Unit.Legion: text = 'L'; break;
+				case Unit.Musketeers: text = 'M'; break;
+				case Unit.Riflemen: text = 'R'; break;
+				case Unit.Cavalry: text = 'c'; break;
+				case Unit.Knights: text = 'K'; break;
+				case Unit.Catapult: text = 'C'; break;
+				case Unit.Cannon: text = 'X'; break;
+				case Unit.Chariot: text = 'W'; break;
+				case Unit.Armor: text = 'a'; break;
+				case Unit.MechInf: text = 'I'; break;
+				case Unit.Artillery: text = 'A'; break;
+				case Unit.Fighter: text = 'F'; break;
+				case Unit.Bomber: text = 'B'; break;
+				case Unit.Trireme: text = 'T'; break;
+				case Unit.Sail: text = 's'; break;
+				case Unit.Frigate: text = 'f'; break;
+				case Unit.Ironclad: text = 'i'; break;
+				case Unit.Cruiser: text = 'Y'; break;
+				case Unit.Battleship: text = 'Z'; break;
+				case Unit.Submarine: text = 'U'; break;
+				case Unit.Carrier: text = 'G'; break;
+				case Unit.Transport: text = 'H'; break;
+				case Unit.Nuclear: text = 'N'; break;
+				case Unit.Diplomat: text = 'D'; break;
+				case Unit.Caravan: text = 't'; break;
+			}
+			output.DrawText(text.ToString(), 0, 8, 8, 5, TextAlign.Center);
+			output.DrawText(text.ToString(), 0, 7, 8, 4, TextAlign.Center);
 			return output;
 		}
 
