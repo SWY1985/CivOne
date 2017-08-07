ebbe61f6d2cc0dae9ce5418618d1c6865095860b
SWY1985 <mail@swouters.nl>
2017-08-07 14:36:26 +0200

    Added DataCheck and custom runtime settings

--- src/RuntimeSettings.cs
+++ src/RuntimeSettings.cs
@@ -7,11 +7,49 @@
 // You should have received a copy of the CC0 legalcode along with this
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+using System.Collections.Generic;
+
 namespace CivOne
 {
 	public class RuntimeSettings
 	{
+		private readonly Dictionary<string, object> _customSettings = new Dictionary<string, object>();
+
 		public bool Demo { get; set; }
 		public bool Setup { get; set; }
+		public bool DataCheck { get; set; }
+
+		public object this[string customSetting]
+		{
+			get
+			{
+				if (_customSettings.ContainsKey(customSetting.ToLower()))
+					return _customSettings[customSetting.ToLower()];
+				return null;
+			}
+			set
+			{
+				if (_customSettings.ContainsKey(customSetting.ToLower()))
+					_customSettings[customSetting.ToLower()] = value;
+				_customSettings.Add(customSetting.ToLower(), value);
+			}
+		}
+
+		public T Get<T>(string customSetting)
+		{
+			try
+			{
+				return (T)this[customSetting];
+			}
+			catch
+			{
+				return default(T);
+			}
+		}
+
+		public RuntimeSettings()
+		{
+			DataCheck = true;
+		}
 	}
 }
