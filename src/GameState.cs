8b6bc2b4c24523ca4f25f1f3e9c31a7c9b859d28
uHappyLogic <kosiak.lukasz@gmail.com>
2017-10-27 18:27:09 +0200

    missing license comment added

--- src/GameState.cs
+++ src/GameState.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.Generic;
+﻿// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+
+using System.Collections.Generic;
 using System.Linq;
 using CivOne.Advances;
 using CivOne.Civilizations;
