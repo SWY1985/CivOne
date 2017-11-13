19be286af9a4cdc6e8eb78a47f14aecbde2f0563
SWY1985 <mail@sw1.nl>
2017-11-13 15:21:49 +0100

    #419 Added SDL2 stub on Linux

--- runtime/sdl/src/Program.cs
+++ runtime/sdl/src/Program.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace CivOne
 {
 	internal class Program
 	{
+		private static bool WriteSdlStub()
+		{
+			string binPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "SDL2.so");
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return false;
+			if (File.Exists(binPath)) return false;
+			foreach (string bla in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+			{
+				Console.WriteLine(bla);
+			}
+			using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CivOne.SDL.Resources.SDL2.so"))
+			{
+				if (resourceStream == null)
+				{
+					Console.WriteLine("Could not read embedded resource for SDL2.so");
+					return false;
+				}
+				
+				using (FileStream sw = new FileStream(Path.Combine(binPath), FileMode.CreateNew, FileAccess.Write))
+				{
+					resourceStream.CopyTo(sw);
+				}
+			}
+			return File.Exists(binPath);
+		}
+
 		static void Main(string[] args)
 		{
+			if (WriteSdlStub())
+			{
+				Console.WriteLine("Written SDL2 library stub...");
+			}
+
 			using (Runtime runtime = new Runtime())
 			using (GameWindow window = new GameWindow(runtime))
 			{
