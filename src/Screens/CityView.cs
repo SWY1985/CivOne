8aace82053f40de740fb9e3d007aeccc254cf12a
SWY1985 <mail@swouters.nl>
2017-11-16 19:46:10 +0100

    Fixed incorrect gold value when caputring city
    This closes #428

--- src/Screens/CityView.cs
+++ src/Screens/CityView.cs
@@ -751,6 +751,7 @@ namespace CivOne.Screens
 				int cityLuxuries = _city.Luxuries;
 				if (cityLuxuries == 0) cityLuxuries = 1;
 				int captureGold = (int)Math.Floor(((float)totalGold / totalLuxuries) * cityLuxuries);
+				if (captureGold < 0) captureGold = 0;
 
 				Game.GetPlayer(_city.Owner).Gold -= (short)captureGold;
 				Game.CurrentPlayer.Gold += (short)captureGold;
