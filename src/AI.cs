36d3db19b4e53cf767c804ae98b6c52f719998dc
SWY1985 <mail@swouters.nl>
2017-11-18 17:03:38 +0100

    AI should not attack using Settlers unit
    This closes #433

--- src/AI.cs
+++ src/AI.cs
@@ -121,6 +121,7 @@ namespace CivOne
 					int relY = Common.Random.Next(-1, 2);
 					if (relX == 0 && relY == 0) continue;
 					if (unit.Tile[relX, relY] is Ocean) continue;
+					if (unit.Tile[relX, relY].Units.Any(x => x.Owner != unit.Owner)) continue;
 					unit.MoveTo(relX, relY);
 					return;
 				}
