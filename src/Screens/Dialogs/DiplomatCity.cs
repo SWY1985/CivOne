c7d2493f87a4d2f269b32cf7e9163de63fb36db1
Christoph Donges <christoph.donges@gen3media.com>
2018-03-16 23:08:13 +1000

    Get incite mostly working

--- src/Screens/Dialogs/DiplomatCity.cs
+++ src/Screens/Dialogs/DiplomatCity.cs
@@ -24,20 +24,35 @@ namespace CivOne.Screens.Dialogs
 		private readonly City _enemyCity;
 		private readonly Diplomat _diplomat;
 
-		private void DontBrbe(object sender, EventArgs args)
+		private void EstablishEmbassy(object sender, EventArgs args)
 		{
 			Cancel();
 		}
 
-		private void Bribe(object sender, EventArgs args)
+		private void InvestigateCity(object sender, EventArgs args)
 		{
-
 			Cancel();
 		}
 
-		private static int InciteCost(BaseUnitLand unitToBribe)
+		private void InciteRevolt(object sender, EventArgs args)
 		{
-			return 0;
+			GameTask.Enqueue(Tasks.Show.DiplomatIncite(_enemyCity, _diplomat));
+			Cancel();
+		}
+
+		private void IndustrialSabotage(object sender, EventArgs args)
+		{
+			Cancel();
+		}
+
+		private void MeetWithKing(object sender, EventArgs args)
+		{
+			Cancel();
+		}
+
+		private void StealTechnology(object sender, EventArgs args)
+		{
+			Cancel();
 		}
 
 		protected override void FirstUpdate()
@@ -54,12 +69,12 @@ namespace CivOne.Screens.Dialogs
 				FontId = FONT_ID
 			};
 
-			menu.Items.Add("Establish Embassy").OnSelect(DontBrbe);
-			menu.Items.Add("InvestigateCity").OnSelect(Bribe);
-			menu.Items.Add("Steal Technology").OnSelect(DontBrbe);
-			menu.Items.Add("Industrial Sabotage").OnSelect(Bribe);
-			menu.Items.Add("Incite a Revolt").OnSelect(DontBrbe);
-			menu.Items.Add("Meet with King").OnSelect(Bribe);
+			menu.Items.Add("Establish Embassy").OnSelect(EstablishEmbassy).Disable();
+			menu.Items.Add("InvestigateCity").OnSelect(InvestigateCity).Disable();
+			menu.Items.Add("Steal Technology").OnSelect(StealTechnology).Disable();
+			menu.Items.Add("Industrial Sabotage").OnSelect(IndustrialSabotage).Disable();
+			menu.Items.Add("Incite a Revolt").OnSelect(InciteRevolt);
+			menu.Items.Add("Meet with King").OnSelect(MeetWithKing).Disable();
 			
 			AddMenu(menu);
 		}
@@ -77,7 +92,7 @@ namespace CivOne.Screens.Dialogs
 			_diplomat = diplomat ?? throw new ArgumentNullException(nameof(diplomat));
 
 			DialogBox.DrawText($"{_enemyCity.Player.TribeName} diplomat arrives", 0, 15, 5, 5);
-			DialogBox.DrawText($"in ${_enemyCity.Name}", 0, 15, 5, 5 + Resources.GetFontHeight(FONT_ID));
+			DialogBox.DrawText($"in {_enemyCity.Name}", 0, 15, 5, 5 + Resources.GetFontHeight(FONT_ID));
 		}
 	}
 }
