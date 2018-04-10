e89070efbcb268f3ef310048152cb977dbd03c7d
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2018-04-10 13:51:42 +0200

    Placed Barbarian movement code in seperate file

---
+++ src/AI.Barbarians.cs
@@ -0,0 +1,139 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+using System.Drawing;
+using System.Linq;
+using CivOne.Enums;
+using CivOne.Tasks;
+using CivOne.Tiles;
+using CivOne.Units;
+
+namespace CivOne
+{
+	internal partial class AI
+	{
+		private static void BarbarianMove(IUnit unit)
+		{
+			switch (unit.Class)
+			{
+				case UnitClass.Water:
+					{
+						if (!unit.Tile.Units.Any(x => x.Class == UnitClass.Land))
+						{
+							Game.DisbandUnit(unit);
+							return;
+						}
+
+						for (int i = 0; i < 1000; i++)
+						{
+							if (unit.Tile.GetBorderTiles().Any(x => !x.IsOcean))
+							{
+								if (Game.GetCities().Any(x => x.Owner != 0))
+								{
+									City nearestCity = Game.GetCities().Where(x => x.Owner != 0).OrderBy(x => Common.DistanceToTile(x.X, x.Y, unit.X, unit.Y)).ThenBy(x => x.Player == Human ? 0 : 1).First();
+									if (nearestCity.Player == Human && Human.Visible(unit.Tile))
+									{
+										GameTask.Insert(Message.Advisor(Advisor.Defense, false, "Barbarian raiding party", $"lands near {nearestCity.Name}!", "Citizens are alarmed."));
+									}
+								}
+
+								foreach (IUnit landUnit in unit.Tile.Units.Where(x => x.Class == UnitClass.Land && x.Sentry))
+								{
+									landUnit.Sentry = false;
+								}
+								unit.SkipTurn();
+								continue;
+							}
+
+							if (unit.Goto.IsEmpty)
+							{
+								if (!Game.GetCities().Any(x => x.Owner != 0)) Game.DisbandUnit(unit);
+								
+								City nearestCity = Game.GetCities().Where(x => x.Owner != 0).OrderBy(x => Common.DistanceToTile(x.X, x.Y, unit.X, unit.Y)).First();
+								if (Common.DistanceToTile(unit.X, unit.Y, nearestCity.X, nearestCity.Y) > 10) Game.DisbandUnit(unit);
+								unit.Goto = new Point(nearestCity.X, nearestCity.Y);
+								continue;
+							}
+
+							if (!unit.Goto.IsEmpty)
+							{
+								int distance = unit.Tile.DistanceTo(unit.Goto);
+								ITile[] tiles = unit.MoveTargets.OrderBy(x => x.DistanceTo(unit.Goto)).ThenBy(x => x.Movement).ToArray();
+								if (tiles.Length == 0 || tiles[0].DistanceTo(unit.Goto) > distance)
+								{
+									// No valid tile to move to, cancel goto
+									unit.Goto = Point.Empty;
+									continue;
+								}
+								else if (tiles[0].DistanceTo(unit.Goto) == distance)
+								{
+									// Distance is unchanged, 50% chance to cancel goto
+									if (Common.Random.Next(0, 100) < 50)
+									{
+										unit.Goto = Point.Empty;
+										continue;
+									}
+								}
+
+								if (!unit.MoveTo(tiles[0].X - unit.X, tiles[0].Y - unit.Y))
+								{
+									unit.Goto = Point.Empty;
+									unit.SkipTurn();
+									return;
+								}
+							}
+
+							unit.SkipTurn();
+							return;
+						}
+					}
+					return;
+				case UnitClass.Land:
+					{
+						ITile[] tiles = unit.Tile.GetBorderTiles().Where(t => !((unit.Tile.IsOcean || unit is Diplomat) && t.City != null) && !t.IsOcean && t.Units.Any(u => u.Owner != 0)).ToArray();
+						if (tiles.Length == 0)
+						{
+							// No adjecent units found
+							if (Common.Random.Next(10) < 7)
+							{
+								for (int i = 0; i < 1000; i++)
+								{
+									int relX = Common.Random.Next(-1, 2);
+									int relY = Common.Random.Next(-1, 2);
+									if (relX == 0 && relY == 0) continue;
+									if (unit.Tile[relX, relY] is Ocean) continue;
+									if (unit is Diplomat && unit.Tile[relX, relY].City != null) continue;
+									if (unit.Tile.IsOcean && unit.Tile[relX, relY].City != null) continue;
+									unit.MoveTo(relX, relY);
+									return;
+								}
+							}
+							Game.DisbandUnit(unit);
+							return;
+						}
+						else
+						{
+							ITile moveTo = tiles[Common.Random.Next(tiles.Length)];
+							int relX = moveTo.X - unit.X;
+							int relY = moveTo.Y - unit.Y;
+							while (relX < -1) relX += 80;
+							while (relX > 1) relX -= 80;
+							if (unit is Diplomat && unit.Tile.City != null) return;
+
+							unit.MoveTo(relX, relY);
+						}
+					}
+					return;
+				default:
+					Game.DisbandUnit(unit);
+					return;
+			}
+		}
+	}
+}
