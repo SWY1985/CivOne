407d7710a2cafdea776d894f310b8d72c5b5ed56
Stefan Wouters-Yaowanasap <s.wouters@advitronic.nl>
2018-08-08 09:43:19 +0200

    Added source header to Resources.cs

--- runtime/sdl/src/Resources.cs
+++ runtime/sdl/src/Resources.cs
@@ -1,3 +1,12 @@
+// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 using System;
 using System.IO;
 using System.Linq;
